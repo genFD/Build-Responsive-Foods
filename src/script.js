4f4e0f31fb3d44681b5794c58d4d638820e9f42f
hermkan <hermann.kanga.dev@gmail.com>
2021-10-22 09:48:43 -0400

    completed markup for section 2

--- src/script.js
+++ src/script.js
@@ -2,15 +2,15 @@
 const icons = document.querySelectorAll('.image-slide-show i');
 let i = 1;
 
-setInterval(() => {
-  i++;
-  const icon = document.querySelector('.image-slide-show .change');
-  icon.classList.remove('change');
-  if (i > icons.length) {
-    // start over and add change class to the first item in the array, the first icon and also set counter back to 1
-    icons[0].classList.add('change');
-    i = 1;
-  } else {
-    icon.nextElementSibling.classList.add('change');
-  }
-}, 4000);
+// setInterval(() => {
+//   i++;
+//   const icon = document.querySelector('.image-slide-show .change');
+//   icon.classList.remove('change');
+//   if (i > icons.length) {
+//     // start over and add change class to the first item in the array, the first icon and also set counter back to 1
+//     icons[0].classList.add('change');
+//     i = 1;
+//   } else {
+//     icon.nextElementSibling.classList.add('change');
+//   }
+// }, 4000);
