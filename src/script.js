e9663b831f51440ace5b24d09009226d856cb710
hermkan <hermann.kanga.dev@gmail.com>
2021-10-23 21:30:42 -0400

    created markup for navbar and styles

--- src/script.js
+++ src/script.js
@@ -1,16 +1,21 @@
-// 1remove icons after 4s
 const icons = document.querySelectorAll('.image-slide-show i');
 let i = 1;
+const menuItem = document.querySelector('.menu');
+const navAndMenu = document.querySelectorAll('.target');
+menuItem.addEventListener('click', () => {
+  navAndMenu.forEach((item) => {
+    item.classList.toggle('change');
+  });
+});
 
-// setInterval(() => {
-//   i++;
-//   const icon = document.querySelector('.image-slide-show .change');
-//   icon.classList.remove('change');
-//   if (i > icons.length) {
-//     // start over and add change class to the first item in the array, the first icon and also set counter back to 1
-//     icons[0].classList.add('change');
-//     i = 1;
-//   } else {
-//     icon.nextElementSibling.classList.add('change');
-//   }
-// }, 4000);
+setInterval(() => {
+  i++;
+  const icon = document.querySelector('.image-slide-show .change');
+  icon.classList.remove('change');
+  if (i > icons.length) {
+    icons[0].classList.add('change');
+    i = 1;
+  } else {
+    icon.nextElementSibling.classList.add('change');
+  }
+}, 4000);
